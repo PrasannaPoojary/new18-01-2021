bb01552ed668367526a50eb263b5c60f3a54dae0
Prasanna <Prasanna@DESKTOP-VIB6UCD>
2022-01-18 21:07:50 +0530

    hI Please find my update as 18/01/2021

---
+++ src/test/java/continurTest/DoSomethingTest.java
@@ -0,0 +1,10 @@
+package continurTest;
+
+public class DoSomethingTest {
+	public static void main(String[] args) {
+		System.out.println("Hello PRASANNA");
+		System.out.println("hOW ARE YOU");
+		System.out.println("gOOD MORNING HAVE A NICE DAY");
+	}
+
+}
